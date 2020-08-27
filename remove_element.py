5c322741993c40e46e814b02244415214ebdb2f9
nishantsharma12 <nishant@navigus.in>
2020-08-27 12:02:43 +0530

    remove element in array

--- remove_element.py
+++ remove_element.py
@@ -4,7 +4,12 @@ def removeElement(nums, val):
     :type val: int
     :rtype: int
     """
-    
+    count = 0
+    for num in nums:
+        if num != val:
+            nums[count] = num
+            count += 1
+    return count 
 
 if __name__ == '__main__':
     nums =[0,1,2,2,3,0,4,2]
